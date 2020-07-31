572587f90b2657742f76b722b9349ff295f7dc54
David Lucas <68432435+davidlucasm81@users.noreply.github.com>
2020-07-31 18:56:08 +0200

    Update PrincipalPanel.java

--- src/MySqlGest/PrincipalPanel.java
+++ src/MySqlGest/PrincipalPanel.java
@@ -23,7 +23,6 @@ public class PrincipalPanel extends JPanel {
     boolean connected = false;
     boolean error = false;
     Color c;
-
     public PrincipalPanel() {
         msj = "Welcome to MySqlGest";
 
@@ -46,13 +45,11 @@ public class PrincipalPanel extends JPanel {
         add(pass);
         add(db);
     }
-
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Font myFont = new Font("Comic Sans", Font.BOLD, 15); // Yep, comic sans
         g.setColor(Color.BLACK);
         g.setFont(myFont);
-
         // Principal Text:
         g.drawString(msj, 135 - msj.length() * 4, 60);
         // First Window:
@@ -63,23 +60,19 @@ public class PrincipalPanel extends JPanel {
             g.drawString("DB: ", 25, 155);
             g.drawString("IP:", 25, 195);
             g.drawString(":", 163, 195);
-
             //Initial TextField:
             user.setBounds(70, 80, 150, 20);
             pass.setBounds(70, 110, 150, 20);
             db.setBounds(70, 140, 150, 20);
-
             //Address TextField:
             localhost.setBounds(100, 210, 90, 12);
             localhost.setBackground(c);
             ip.setBounds(70, 180, 90, 20);
             port.setBounds(170, 180, 50, 20);
-
             //Initial Button:
             buttonSignIn.setBounds(100, 240, 80, 21);
         }
         // Disconnect Button:
         disconnect.setBounds(90, 220, 100, 21);
-
     }
 }
